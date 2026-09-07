b19bf261a5b863acac76751015b95343adb98379
Moarari <matko.2010.matko@gmail.com>
2026-09-07 09:32:50 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void) {
+    printf("Zadaj cislo\n");
+    int sup;
+    scanf("%d", &sup);
+    printf("Zadal si cislo %d\n",sup);
+    
+    
+    int y = sup;
+    char s = 'o';
+    for (int i=0; i< 10; i++) {
+        
+        int x = 10;
+        printf("%c", s);
+
+    }
+
+
+    printf("Ahoj \n");
+
+    return 0;
+}
